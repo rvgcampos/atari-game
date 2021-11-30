83efc62001047a77b7eb796c6fe10a7d2c427e01
paulo-zx <paulo-zx@hotmail.com>
2021-11-30 14:58:37 -0300

    player 2 movimento

--- hockey_game.asm
+++ hockey_game.asm
@@ -1,5 +1,4 @@
-
-	processor 6502
+processor 6502
     include "vcs.h"
     include "macro.h"  
 
@@ -10,6 +9,7 @@
     seg.u Variables
     org $80
 P0XPos byte                 ; Sprite X coordinate 
+P1XPos byte                 ; Sprite X coordinate 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Começo da ROM em $F000
@@ -31,6 +31,9 @@ Start:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #30
     sta P0XPos     ; initialize player X coordinate
+
+    lda #110
+    sta P1XPos     ; initialize player X coordinate
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Renderizar o frame
@@ -73,11 +76,31 @@ DivideLoop:
     sta WSYNC      ; Espera scanline
     sta HMOVE      ; Aplica a posição fina no registrador
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    lda P1XPos     ; Registrador A contém a posição no eixo-x desejada
+    and #$7F       ; Operação AND com $7 para o valor ser positivo
+    sta WSYNC      ; Espera scanline
+    sta HMCLR      ; Limpa valores de posições horizontais
+    sec            ; Setando a flag de carry antes da subtração
+DivideLoop1:
+    sbc #15        ; Subtrai 15 do do registrador A
+    bcs DivideLoop1 ; Faz o loop enquanto a flag de carry está setada (ajuste GROSSO)
+
+    eor #7         ; Ajusta o resto (da divisão) pra ficar entre -8 e 7 (ajuste FINO)
+    asl            ; Operação de shift pois o registrador HMP0 apenas usa 4 bits
+    asl
+    asl
+    asl
+    sta HMP1       ; (Ajuste FINO)
+    sta RESP1      ; 
+    sta WSYNC      ; Espera scanline
+    sta HMOVE      ; Aplica a posição fina no registrador
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Setar as próximas 35 linhas do VBLANK
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    REPEAT 35
+    REPEAT 33
         sta WSYNC
     REPEND
     lda #0
@@ -127,9 +150,13 @@ DivideLoop:
 DrawBitmap:
     lda P0Bitmap,Y ; load player bitmap slice of data
     sta GRP0       ; set graphics for player 0 slice
-
     lda P0Color,Y  ; load player color from lookup table
     sta COLUP0     ; set color for player 0 slice
+
+    lda P1Bitmap,Y ; load player bitmap slice of data
+    sta GRP1       ; set graphics for player 0 slice
+    lda P1Color,Y  ; load player color from lookup table
+    sta COLUP1     ; set color for player 0 slice
 
     sta WSYNC      ; wait for next scanline
 
@@ -138,14 +165,8 @@ DrawBitmap:
 
     lda #0
     sta GRP0       ; disable P0 bitmap graphics
+    sta GRP1       ; disable P0 bitmap graphics
 
-    ; Lateral do playfield
-    /* ldx #%00100000
-    stx PF0
-    ldx #%00000000
-    stx PF1
-    ldx #%10000000
-    stx PF2 */
     REPEAT 66
         sta WSYNC
     REPEND
@@ -159,7 +180,6 @@ DrawBitmap:
     REPEAT 7
         sta WSYNC
     REPEND
-
 
     ; Pulando 14 linhas, ou seja, tornando as preto
     ldx #0
@@ -181,11 +201,75 @@ DrawBitmap:
     lda #0
     sta VBLANK
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Input para o player 0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+CheckP0Up:
+    lda #%00010000
+    bit SWCHA
+    bne CheckP0Down
+    inc P0XPos
+
+CheckP0Down:
+    lda #%00100000
+    bit SWCHA
+    bne CheckP0Left
+    dec P0XPos
+
+CheckP0Left:
+    lda #%01000000
+    bit SWCHA
+    bne CheckP0Right
+    dec P0XPos
+
+CheckP0Right:
+    lda #%10000000
+    bit SWCHA
+    bne NoInput
+    inc P0XPos
+
+NoInput:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Input para o player 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Input para o player 0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+CheckP1Up:
+    lda #%00000001
+    bit SWCHA
+    bne CheckP1Down
+    inc P1XPos
+
+CheckP1Down:
+    lda #%00000010
+    bit SWCHA
+    bne CheckP1Left
+    dec P1XPos
+
+CheckP1Left:
+    lda #%00000100
+    bit SWCHA
+    bne CheckP1Right
+    dec P1XPos
+
+CheckP1Right:
+    lda #%00001000
+    bit SWCHA
+    bne NoInput1
+    inc P1XPos
+
+NoInput1:
+    
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Pular para o proximo frame 
+;;; Pular para o proximo frame 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     jmp NextFrame
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 P0Bitmap:
@@ -199,10 +283,31 @@ P0Bitmap:
     .byte #%01000000;$42
     .byte #%01000000;$3A
 
+P1Bitmap:
+    .byte #%10000110;$80
+    .byte #%01000010;$80
+    .byte #%00100010;$80
+    .byte #%00010010;$70
+    .byte #%00001010;$42
+    .byte #%00000110;$42
+    .byte #%00000010;$42
+    .byte #%00000010;$3A
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Lookup table for the player colors
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 P0Color:
+    .byte #$00;
+    .byte #$80;
+    .byte #$80;
+    .byte #$80;
+    .byte #$70;
+    .byte #$42;
+    .byte #$42;
+    .byte #$42;
+    .byte #$3A;
+
+P1Color:
     .byte #$00;
     .byte #$80;
     .byte #$80;
